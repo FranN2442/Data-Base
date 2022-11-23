53aaa89911f75c231452385a1b86ec58dea12164
FranN2442 <118172581+FranN2442@users.noreply.github.com>
2022-11-23 22:37:45 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,34 @@
+import uuid
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class Bike(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    title: str = Field(...)
+    author: str = Field(...)
+    synopsis: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
+                "title": "Mountain Bike",
+                "author": "Francisco Javier",
+                "synopsis": "..."
+            }
+        }
+
+class BikeUpdate(BaseModel):
+    title: Optional[str]
+    author: Optional[str]
+    synopsis: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Mountain Bike",
+                "author": "Francisco Javier",
+                "synopsis": "This bike is the best for roads in the mountains..."
+            }
+        }
